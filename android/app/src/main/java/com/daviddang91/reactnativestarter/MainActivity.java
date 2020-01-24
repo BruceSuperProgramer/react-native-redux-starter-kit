e99a8b3f48b1cbd5291e49f9212cd42c823bcb7d
bruce <programerbruce@gmail.com>
2020-01-25 02:29:51 +1100

    ⛏Changes been made accroding to comparision between o.57 to 0.615 git diff

--- android/app/src/main/java/com/daviddang91/reactnativestarter/MainActivity.java
+++ android/app/src/main/java/com/daviddang91/reactnativestarter/MainActivity.java
@@ -4,15 +4,14 @@ import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "ReactNativeStarter";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ReactNativeStarter";
+  }
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
